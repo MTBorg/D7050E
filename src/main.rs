7f90471db313a27468288557ad426a086c02b0d5
MTBorg <martinterneborg96@gmail.com>
2019-09-04 11:08:06 +0200

    Implement addition in the AST

--- src/main.rs
+++ src/main.rs
@@ -1,70 +1,69 @@
 extern crate nom;
 
 use nom::{
+    branch::alt, bytes::complete::tag, character::complete::space0, number::complete::double,
     IResult,
-    bytes::complete::{tag},
-    number::complete::double,
-    branch::alt
 };
 
-enum NODE_VALUE{
+#[derive(Debug)]
+enum NODE_VALUE {
     numeric(f64),
-    operator(char)
+    operator(char),
 }
 
-struct Node{
+#[derive(Debug)]
+struct Node {
     pub element: NODE_VALUE,
     pub left_child: Option<Box<Node>>,
-    pub right_child: Option<Box<Node>>
+    pub right_child: Option<Box<Node>>,
 }
 
-fn parse_number(s: &str) -> IResult<&str, f64>{
+fn parse_number(s: &str) -> IResult<&str, f64> {
     double(s)
 }
 
-fn parse_op(s: &str) -> IResult<&str, &str>{
-    alt((
-        tag("+"),
-        tag("-"),
-        tag("/"),
-        tag("*")
-    ))(s)
+fn parse_op(s: &str) -> IResult<&str, &str> {
+    alt((tag("+"), tag("-"), tag("/"), tag("*")))(s)
 }
 
-fn parse_expr(s: &str, root: Node, last_op: Option<char>) -> Node{
+fn remove_whitespace(s: &str) -> IResult<&str, &str> {
+    space0(s)
+}
+
+fn parse_expr(s: &str) -> Node {
     // Get the number and operator
-    if s.len() == 0{
+    if s.len() == 0 {
         panic!("Received empty expression");
     }
 
-    let (s, num)= parse_number(s).unwrap();
+    let s = remove_whitespace(s).unwrap().0;
+    let (s, num) = parse_number(s).unwrap();
 
-    println!("num:{}", num);
     // Make sure that there is an op left to parse (needed at the end of a string)
-    if s.len() == 0{
-        println!("EMPTY STRING");
-        return root
+    if s.len() == 0 {
+        return Node {
+            element: NODE_VALUE::numeric(num),
+            left_child: None,
+            right_child: None,
+        };
     }
-    let (s, op)=parse_op(s).unwrap();
-    println!("op:{}", op);
+
+    let s = remove_whitespace(s).unwrap().0;
+    let (s, op) = parse_op(s).unwrap();
 
     let op = op.chars().next().unwrap();
 
-    // // Check if we can close the parenthesis
-    // if (op == '*' || op == '/') && (last_op == Some('+') || last_op == Some('-')){
-    //     root.right_child = parse_expr 
-    // }
-
-    // let last_op = Some(op);
-
-    parse_expr(s, root, Some(op))
+    return Node {
+        element: NODE_VALUE::operator(op),
+        left_child: Some(Box::new(Node {
+            element: NODE_VALUE::numeric(num),
+            left_child: None,
+            right_child: None,
+        })),
+        right_child: Some(Box::new(parse_expr(s))),
+    };
 }
 
 fn main() {
-    let ast_root: Node = Node{
-        element: NODE_VALUE::operator('*'),
-        left_child: None,
-        right_child: None
-    };
-    parse_expr("10*1*3+4", ast_root, None);
+    println!("{:#?}", parse_expr("1+ 2+3+ 4"));
 }
