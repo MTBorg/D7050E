5b0e2efb985efe5bb0448c205cae15c2bc8ca02d
MTBorg <martinterneborg96@gmail.com>
2019-09-07 20:57:14 +0200

    Fix lalrpop mods not being found

--- build.rs
+++ build.rs
@@ -1,5 +1,8 @@
 extern crate lalrpop;
 
 fn main() {
-    lalrpop::process_root().unwrap();
+    // Set the input directory for lalrpop-files to src/grammar
+    let mut lalrpop_config = lalrpop::Configuration::new();
+    lalrpop_config.set_in_dir("src/grammar/");
+    lalrpop_config.process().unwrap()
 }
