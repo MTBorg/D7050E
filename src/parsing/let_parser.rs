9eb191de50b496b5dc4218c8540dd1bfdc3be94c
MTBorg <martinterneborg96@gmail.com>
2019-09-09 21:19:10 +0200

    Make the let parser look for ending semi-colons

--- src/parsing/let_parser.rs
+++ src/parsing/let_parser.rs
@@ -15,51 +15,51 @@ mod tests{
 
     #[test]
     fn test_parser_let_no_type_int(){
-        assert!(LetParser::new().parse("let a = 2").is_ok());
+        assert!(LetParser::new().parse("let a = 2;").is_ok());
     }
 
     #[test]
     fn test_parser_let_no_type_float(){
-        assert!(LetParser::new().parse("let a = 1.5").is_ok());
+        assert!(LetParser::new().parse("let a = 1.5;").is_ok());
     }
 
     #[test]
     fn test_parser_let_no_type_negative(){
-        assert!(LetParser::new().parse("let a = -11").is_ok());
+        assert!(LetParser::new().parse("let a = -11;").is_ok());
     }
 
     #[test]
     fn test_parser_let_type_i32(){
-        assert!(LetParser::new().parse("let a: i32 = 2").is_ok());
+        assert!(LetParser::new().parse("let a: i32 = 2;").is_ok());
     }
 
     #[test]
     fn test_parser_let_type_i32_negative(){
-        assert!(LetParser::new().parse("let a: i32 = -50").is_ok());
+        assert!(LetParser::new().parse("let a: i32 = -50;").is_ok());
     }
 
     #[test]
     fn test_parser_let_type_f32(){
-        assert!(LetParser::new().parse("let a: f32 = 2.0").is_ok());
+        assert!(LetParser::new().parse("let a: f32 = 2.0;").is_ok());
     }
 
     #[test]
     fn test_parser_let_missing_colon(){
-        assert!(!LetParser::new().parse("let a: = 7").is_ok());
+        assert!(!LetParser::new().parse("let a: = 7;").is_ok());
     }
 
     #[test]
     fn test_parser_let_missing_value(){
-        assert!(!LetParser::new().parse("let a: f32 = ").is_ok());
+        assert!(!LetParser::new().parse("let a: f32 = ;").is_ok());
     }
 
     #[test]
     fn test_parser_let_missing_comma_and_value(){
-        assert!(!LetParser::new().parse("let a f32 = ").is_ok());
+        assert!(!LetParser::new().parse("let a f32 = ;").is_ok());
     }
 
     #[test]
     fn test_parser_let_missing_equal_sign(){
-        assert!(!LetParser::new().parse("let a: i32 4").is_ok());
+        assert!(!LetParser::new().parse("let a: i32 4;").is_ok());
     }
 }
