bc2509299dff779c978442062c8ad7003caf961d
MTBorg <martinterneborg96@gmail.com>
2019-09-09 21:25:45 +0200

    Make expr-parser tests use wrapper function

--- src/parsing/expr_parser.rs
+++ src/parsing/expr_parser.rs
@@ -17,66 +17,66 @@ pub fn parse(s: &str) -> Result<Box<Node>, ParseError>{
 
 #[cfg(test)]
 mod tests{
-    use crate::parsing::grammar::ExprParser;
+    use super::parse;
 
     #[test]
     fn test_parse_number_no_parens() {
-        assert!(ExprParser::new().parse("123").is_ok());
+        assert!(parse("123").is_ok());
     }
 
     #[test]
     fn test_parse_number_parens() {
-        assert!(ExprParser::new().parse("(458)").is_ok());
+        assert!(parse("(458)").is_ok());
     }
 
     #[test]
     fn test_parse_var_no_parens() {
-        assert!(ExprParser::new().parse("abc").is_ok());
+        assert!(parse("abc").is_ok());
     }
 
     #[test]
     fn test_parse_var_parens() {
-        assert!(ExprParser::new().parse("(abc)").is_ok());
+        assert!(parse("(abc)").is_ok());
     }
 
     #[test]
     fn test_parse_var_trailing_number() {
-        assert!(ExprParser::new().parse("x3").is_ok());
+        assert!(parse("x3").is_ok());
     }
 
     #[test]
     fn test_parse_var_no_starting_number() {
-        assert!(!ExprParser::new().parse("(234var)").is_ok());
+        assert!(!parse("(234var)").is_ok());
     }
 
     #[test]
     fn test_parse_var_no_space() {
-        assert!(!ExprParser::new().parse("var1 var2").is_ok());
+        assert!(!parse("var1 var2").is_ok());
     }
 
     #[test]
     fn test_parse_correct_expressions() {
-        assert!(ExprParser::new().parse("1+2+3").is_ok());
-        assert!(ExprParser::new().parse("1 * 3 + 2 / 1").is_ok());
-        assert!(ExprParser::new().parse("1 +    2   ").is_ok());
-        assert!(ExprParser::new().parse("a + b").is_ok()); // Var + var
-        assert!(ExprParser::new().parse("a - b").is_ok()); // Var - var
-        assert!(ExprParser::new().parse("a * b").is_ok()); // Var * var
-        assert!(ExprParser::new().parse("a / b").is_ok()); // Var / var
-        assert!(ExprParser::new().parse("1 + a").is_ok()); // Literal + var
-        assert!(ExprParser::new().parse("2 - a").is_ok()); // Literal - var
-        assert!(ExprParser::new().parse("5 * a").is_ok()); // Literal * var
-        assert!(ExprParser::new().parse("7 / a").is_ok()); // Literal / var
-        assert!(ExprParser::new().parse("a + 1").is_ok()); // Var + Literal
-        assert!(ExprParser::new().parse("a - 2").is_ok()); // Var - Literal
-        assert!(ExprParser::new().parse("a * 3").is_ok()); // Var * Literal
-        assert!(ExprParser::new().parse("a / 4").is_ok()); // Var / Literal
+        assert!(parse("1+2+3").is_ok());
+        assert!(parse("1 * 3 + 2 / 1").is_ok());
+        assert!(parse("1 +    2   ").is_ok());
+        assert!(parse("a + b").is_ok()); // Var + var
+        assert!(parse("a - b").is_ok()); // Var - var
+        assert!(parse("a * b").is_ok()); // Var * var
+        assert!(parse("a / b").is_ok()); // Var / var
+        assert!(parse("1 + a").is_ok()); // Literal + var
+        assert!(parse("2 - a").is_ok()); // Literal - var
+        assert!(parse("5 * a").is_ok()); // Literal * var
+        assert!(parse("7 / a").is_ok()); // Literal / var
+        assert!(parse("a + 1").is_ok()); // Var + Literal
+        assert!(parse("a - 2").is_ok()); // Var - Literal
+        assert!(parse("a * 3").is_ok()); // Var * Literal
+        assert!(parse("a / 4").is_ok()); // Var / Literal
     }
 
     #[test]
     fn test_parse_incorrect_expressions() {
-        assert!(!ExprParser::new().parse("+1 - 2").is_ok()); // Prefixed operator
-        assert!(!ExprParser::new().parse("1++2").is_ok()); // Double operator
-        assert!(!ExprParser::new().parse("1 + ! 2").is_ok()); // Unknown operator
+        assert!(!parse("+1 - 2").is_ok()); // Prefixed operator
+        assert!(!parse("1++2").is_ok()); // Double operator
+        assert!(!parse("1 + ! 2").is_ok()); // Unknown operator
     }
 }
