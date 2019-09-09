cc457de275cdf5a642df494dd69084001f7d1390
MTBorg <martinterneborg96@gmail.com>
2019-09-09 21:27:13 +0200

    Make function call-parser tests use wrapper function

--- src/parsing/func_call_parser.rs
+++ src/parsing/func_call_parser.rs
@@ -12,60 +12,60 @@ pub fn parse(s: &str) -> Result<Box<Node>, ParseError>{
 
 #[cfg(test)]
 mod tests{
-    use super::FuncCallParser;
+    use super::parse;
 
     #[test]
     pub fn test_no_arguments(){
-        assert!(FuncCallParser::new().parse("foo()").is_ok());
+        assert!(parse("foo()").is_ok());
     }
 
     #[test]
     pub fn test_no_parenthesis_no_arguments(){
-        assert!(!FuncCallParser::new().parse("foo").is_ok());
+        assert!(!parse("foo").is_ok());
     }
 
     #[test]
     pub fn test_no_parenthesis_with_arguments(){
-        assert!(!FuncCallParser::new().parse("foo 123, abc ").is_ok());
+        assert!(!parse("foo 123, abc ").is_ok());
     }
 
     #[test]
     pub fn test_arguments_single_var(){
-        assert!(FuncCallParser::new().parse("foo(bar)").is_ok());
+        assert!(parse("foo(bar)").is_ok());
     }
 
     #[test]
     pub fn test_arguments_single_numeric(){
-        assert!(FuncCallParser::new().parse("foo(123)").is_ok());
+        assert!(parse("foo(123)").is_ok());
     }
 
     #[test]
     pub fn test_arguments_multiple_var(){
-        assert!(FuncCallParser::new().parse("foo(bar, var1)").is_ok());
+        assert!(parse("foo(bar, var1)").is_ok());
     }
 
     #[test]
     pub fn test_arguments_multiple_numeric(){
-        assert!(FuncCallParser::new().parse("foo(123, 456)").is_ok());
+        assert!(parse("foo(123, 456)").is_ok());
     }
 
     #[test]
     pub fn test_arguments_numerics_and_vars(){
-        assert!(FuncCallParser::new().parse("foo(123, abc, 456, 789, bar)").is_ok());
+        assert!(parse("foo(123, abc, 456, 789, bar)").is_ok());
     }
 
     #[test]
     pub fn test_missing_argument(){
-        assert!(!FuncCallParser::new().parse("foo(123, , bar)").is_ok());
+        assert!(!parse("foo(123, , bar)").is_ok());
     }
 
     #[test]
     pub fn test_missing_commas(){
-        assert!(!FuncCallParser::new().parse("foo(123 xyz bar)").is_ok());
+        assert!(!parse("foo(123 xyz bar)").is_ok());
     }
 
     #[test]
     pub fn test_trailing_comma(){
-        assert!(!FuncCallParser::new().parse("foo(a, b,)").is_ok());
+        assert!(!parse("foo(a, b,)").is_ok());
     }
 }
