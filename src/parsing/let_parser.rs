71b02d069f756bc1630b587c723e89ce9e98b0c5
MTBorg <martinterneborg96@gmail.com>
2019-09-09 20:49:00 +0200

    Add tests for let parser

--- src/parsing/let_parser.rs
+++ src/parsing/let_parser.rs
@@ -8,3 +8,58 @@ pub fn parse(s: &str) -> Result<Box<Node>, ParseError>{
         Err(e) => Err(ParseError{message: e.to_string()}),
     }
 }
+
+#[cfg(test)]
+mod tests{
+    use crate::parsing::grammar::LetParser;
+
+    #[test]
+    fn test_parser_let_no_type_int(){
+        assert!(LetParser::new().parse("let a = 2").is_ok());
+    }
+
+    #[test]
+    fn test_parser_let_no_type_float(){
+        assert!(LetParser::new().parse("let a = 1.5").is_ok());
+    }
+
+    #[test]
+    fn test_parser_let_no_type_negative(){
+        assert!(LetParser::new().parse("let a = -11").is_ok());
+    }
+
+    #[test]
+    fn test_parser_let_type_i32(){
+        assert!(LetParser::new().parse("let a: i32 = 2").is_ok());
+    }
+
+    #[test]
+    fn test_parser_let_type_i32_negative(){
+        assert!(LetParser::new().parse("let a: i32 = -50").is_ok());
+    }
+
+    #[test]
+    fn test_parser_let_type_f32(){
+        assert!(LetParser::new().parse("let a: f32 = 2.0").is_ok());
+    }
+
+    #[test]
+    fn test_parser_let_missing_colon(){
+        assert!(!LetParser::new().parse("let a: = 7").is_ok());
+    }
+
+    #[test]
+    fn test_parser_let_missing_value(){
+        assert!(!LetParser::new().parse("let a: f32 = ").is_ok());
+    }
+
+    #[test]
+    fn test_parser_let_missing_comma_and_value(){
+        assert!(!LetParser::new().parse("let a f32 = ").is_ok());
+    }
+
+    #[test]
+    fn test_parser_let_missing_equal_sign(){
+        assert!(!LetParser::new().parse("let a: i32 4").is_ok());
+    }
+}
