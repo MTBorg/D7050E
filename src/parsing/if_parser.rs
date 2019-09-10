532c18e21c67f5497cff22718049fe0906e1783a
MTBorg <martinterneborg96@gmail.com>
2019-09-10 13:27:20 +0200

    ADd tests for if parser

--- src/parsing/if_parser.rs
+++ src/parsing/if_parser.rs
@@ -8,3 +8,77 @@ pub fn parse(s: &str) -> Result<Box<Node>, ParseError>{
         Err(e) => Err(ParseError{message: e.to_string()}),
     }
 }
+
+#[cfg(test)]
+mod tests{
+    use super::parse;
+
+    #[test]
+    pub fn test_if_parser_if_var(){
+        assert!(parse("
+        if a {
+            let a = 2;
+        }
+        ").is_ok());
+    }
+
+    #[test]
+    pub fn test_if_parser_empty_body(){
+        assert!(parse("
+        if a {
+        }
+        ").is_ok());
+    }
+
+    #[test]
+    pub fn test_if_parser_no_condition(){
+        assert!(!parse("
+        if {
+            let a = 1;
+        }
+        ").is_ok());
+    }
+
+    #[test]
+    pub fn test_if_parser_nested_ifs(){
+        assert!(parse("
+        if a {
+            let d = 2;
+            if b {
+                let c = 1;
+            }
+            let e = 3;
+        }
+        ").is_ok());
+    }
+
+    #[test]
+    pub fn test_if_parser_if_else(){
+        assert!(parse("
+        if a {
+            let a = 2;
+        } else {
+            let b = 3;
+        }
+        ").is_ok());
+    }
+
+    #[test]
+    pub fn test_if_parser_if_else_nested(){
+        assert!(parse("
+        if a {
+            let a = 2;
+            if b {
+                let c = 3;
+            } else{
+                let a = 4;
+            }
+        } else {
+            let b = 3;
+            if d{
+                let a = 4;
+            }
+        }
+        ").is_ok());
+    }
+}
