442d46e62426421bb6b65fcddc3e54d92db7e550
MTBorg <martinterneborg96@gmail.com>
2019-09-10 11:20:04 +0200

    Add body parser tests

--- src/parsing/body_parser.rs
+++ src/parsing/body_parser.rs
@@ -8,3 +8,96 @@ pub fn parse(s: &str) -> Result<Box<Node>, ParseError>{
         Err(e) => Err(ParseError{message: e.to_string()}),
     }
 }
+
+#[cfg(test)]
+mod tests{
+    use super::parse;
+
+    #[test]
+    pub fn test_body_parser_one_instruction(){
+        assert!(parse("
+            {
+                let a = 2;
+            }
+        ").is_ok());
+    }
+
+    #[test]
+    pub fn test_body_parser_two_instructions(){
+        assert!(parse("
+            {
+                let a = 2;
+                let b = 3;
+            }
+        ").is_ok());
+    }
+
+    #[test]
+    pub fn test_body_parser_three_instructions(){
+        assert!(parse("
+            {
+                let a = 3 + 1;
+                let b = a - 2;
+                let c = 1000 -240;
+            }
+        ").is_ok());
+    }
+
+    #[test]
+    pub fn test_body_parser_multiple_statements_one_line(){
+        assert!(parse("
+            {
+                let a = 3 + 1; let b = a - 2; let c = 1000 -240;
+            }
+        ").is_ok());
+    }
+
+    #[test]
+    pub fn test_body_parser_missing_start_bracket(){
+        assert!(!parse("
+                let a = 3 + 1;
+                let c = 1000 -240;
+            }
+        ").is_ok());
+    }
+
+    #[test]
+    pub fn test_body_parser_missing_trailing_bracket(){
+        assert!(!parse("
+            {
+                let a = 3 + 1;
+                let b = a - 2;
+                let c = 1000 -240;
+        ").is_ok());
+    }
+
+    #[test]
+    pub fn test_body_parser_missing_semicolon_first_line(){
+        assert!(!parse("
+            {
+                let a = 3 + 1
+                let b = a - 2;
+                let c = 1000 -240;
+        ").is_ok());
+    }
+
+    #[test]
+    pub fn test_body_parser_missing_semicolon_middle_line(){
+        assert!(!parse("
+            {
+                let a = 3 + 1;
+                let b = a - 2
+                let c = 1000 -240;
+        ").is_ok());
+    }
+
+    #[test]
+    pub fn test_body_parser_missing_semicolon_last_line(){
+        assert!(!parse("
+            {
+                let a = 3 + 1;
+                let b = a - 2;
+                let c = 1000 -240
+        ").is_ok());
+    }
+}
