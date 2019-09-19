40f7506c98356098737731fff084b5b62d424da3
MTBorg <martinterneborg96@gmail.com>
2019-09-19 20:06:45 +0200

    (WIP) Working on interpreter

--- src/interpreter.rs
+++ src/interpreter.rs
@@ -12,10 +12,26 @@ pub fn eval(node: Node) -> Node {
                 Opcode::Sub => eval(*left_node) - eval(*right_node),
                 Opcode::Mul => eval(*left_node) * eval(*right_node),
                 Opcode::Div => eval(*left_node) / eval(*right_node),
+                Opcode::Eq => Node::Bool(eval(*left_node) == eval(*right_node)),
                 _ => panic!("Unknown opcode: {:?}", op)
             }
         },
-        Node::Empty => panic!("Empty node"),
+        Node::If(expr, then_body, else_body, next_instr) => {
+            if eval(*expr) == Node::Bool(true) {
+                eval(*then_body)
+            } else {
+                match else_body {
+                    Some(body) => {
+                        eval(*body)
+                    },
+                    None => match next_instr {
+                        Some(instr) => eval(*instr),
+                        None => eval(Node::Empty)
+                    }
+                }
+            }
+        },
+        Node::Empty => Node::Empty,
         _ => panic!("Unknown nodetype")
     }
 }
